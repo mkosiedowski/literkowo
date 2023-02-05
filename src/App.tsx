67eadf5a964fb0008f0f140e34de35d3c29c9e88
Maciej Kosiedowski <mkosied@gmail.com>
2023-02-05 19:23:23 +0100

    Add share icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import GameStartForm from "./GameStartForm";
 import Game from "./game";
 import './App.css';
+import ShareIcon from "./ShareIcon";
 
 const LENGTH = 5;
 
@@ -32,8 +33,21 @@ function App() {
         component = <Game correctWord={word} />
     }
 
+    const onShareClick = () => {
+        if (navigator.share) {
+            navigator.share({
+                title: 'Literkowo',
+                text: 'Zgadnij moje hasło!',
+                url: window.location.href,
+            })
+            .then(() => console.log('Successful share'))
+            .catch((error) => console.log('Error sharing', error));
+        }
+    }
+
     return <div className="App">
         <h1>Literkowo</h1>
+        <div onClick={onShareClick} className="shareButton"><ShareIcon size={20} /></div>
         {component}
     </div>
 }
